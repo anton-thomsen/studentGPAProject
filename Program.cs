09d34a6dfca8023430ff652bd407cd2b99fc57d6
anton-thomsen <anton.thomsen0@gmail.com>
2025-12-17 21:30:54 +0100

    Arrays with scores for all students

--- Program.cs
+++ Program.cs
@@ -1,38 +1,49 @@
-﻿
-// initialize variables - graded assignments 
+﻿// initialize variables - graded assignments 
+using System.Runtime.CompilerServices;
+
 int currentAssignments = 5;
 
+//Sophias Exam scores and total score
 int [] sophiaExams = {90, 86, 87, 98, 100};
+int sum = 0;
+foreach (int score in sophiaExams)
+{
+    sum += score;
+}
+decimal sophiaTotal = (decimal) sum / currentAssignments;
+
+//Andrews Exam scores and total score
 int [] andrewExams = {92, 89, 81, 96, 90};
+sum = 0;
+foreach (int score in andrewExams)
+{
+    sum += score;
+}
+decimal andrewTotal = (decimal) sum / currentAssignments;
+
+//Emmas Exam scores and total score
 int [] emmaExams = {90, 85, 87, 98, 68};
+sum = 0;
+foreach (int score in emmaExams)
+{
+    sum += score;
+}
+decimal emmaTotal = (decimal) sum / currentAssignments;
+
+//Logans Exam scores and total score
 int [] loganExams = {90, 95, 87, 88, 68};
-
-
-int sophiaSum = 0;
-int andrewSum = 0;
-int emmaSum = 0;
-int loganSum = 0;
-
-decimal sophiaScore;
-decimal andrewScore;
-decimal emmaScore;
-decimal loganScore;
-
-sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
-emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
-loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
-
-sophiaScore = (decimal)sophiaSum / currentAssignments;
-andrewScore = (decimal)andrewSum / currentAssignments;
-emmaScore = (decimal)emmaSum / currentAssignments;
-loganScore = (decimal)loganSum / currentAssignments;
+sum = 0;
+foreach (int score in loganExams)
+{
+    sum += score;
+}
+decimal loganTotal = (decimal) sum / currentAssignments;
 
 Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+Console.WriteLine("Sophia:\t\t" + sophiaTotal + "\tA-");
+Console.WriteLine("Andrew:\t\t" + andrewTotal + "\tB+");
+Console.WriteLine("Emma:\t\t" + emmaTotal + "\tB");
+Console.WriteLine("Logan:\t\t" + loganTotal + "\tA-");
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
--- obj/Debug/net10.0/StudentGPAProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/StudentGPAProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentGPAProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0514164b5b0a49192fd369323b7f6f2942dbf86d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentGPAProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentGPAProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
